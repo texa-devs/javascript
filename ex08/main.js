36677033867e42a1c9785dfffb6e7ed3d66e1d19
texa <alexckape9@gmail.com>
2022-11-23 19:06:19 -0300

    conditionals and equal signs
    conditionals and equal signs explained and commented

---
+++ ex08/main.js
@@ -0,0 +1,10 @@
+//Equal signs and conditional
+
+//in this case we have a double equal sign. That means that it won't match the data type in the first attribution, meaning it can be even a string and it won't matter.
+
+const x = 10;
+
+if(x == 10) {
+    console.log('x is 10')
+}
+
